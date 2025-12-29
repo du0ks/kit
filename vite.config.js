2ebf994d88f259d041044e0812a6f6759d3f75e5
du0ks <du0ks@yandex.com>
2025-12-29 23:27:00 +0300

    encrypted

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/repo-name/', // TODO: Uncomment and set to your repo name for GitHub Pages
+  base: '/kit/',
 })
